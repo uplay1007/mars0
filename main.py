1486b9adaf1e820121dc76d547a313a5e2b34c2f
uplay1007 <118464329+uplay1007@users.noreply.github.com>
2023-04-01 17:10:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -67,14 +67,30 @@ def choice(planet_name):
                           </head>
                           <body>
                             <h1 class="text-primary">Мое предложение: {planet_name}</h1>
-                            <h3 class="text-default" style="background-color:  #C3D099; font-size: 16px;">Пригодна для жизни.</h1>
-                            <h3 class="text-default" style="background-color:  #d63384; font-size: 16px;">Много необходимых ресурсов.</h1>
-                            <h3 class="text-default" style="background-color:  #fd7e14; font-size: 16px;">Близко находится к земле.</h1>
-                            <h3 class="text-default" style="background-color:  #198754; font-size: 16px;">Магнитное поле сравнимо с Землей.</h1>
-                            <h3 class="text-default" style="background-color:  #dc3545; font-size: 16px;">Возможно переселение.</h1>
+                            <h3 class="text-default" style="background-color:  #C3D099; font-size: 16px;">Пригодна для жизни.</h3>
+                            <h3 class="text-default" style="background-color:  #d63384; font-size: 16px;">Много необходимых ресурсов.</h3>
+                            <h3 class="text-default" style="background-color:  #fd7e14; font-size: 16px;">Близко находится к земле.</h3>
+                            <h3 class="text-default" style="background-color:  #198754; font-size: 16px;">Магнитное поле сравнимо с Землей.</h3>
+                            <h3 class="text-default" style="background-color:  #dc3545; font-size: 16px;">Возможно переселение.</h3>
                           </body>
                         </html>"""
 
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <title>Результаты</title>
+                          </head>
+                          <body>
+                            <h1 class="text-primary">Результаты отбора</h1>
+                            <h2 class="text-default">Претендента на участие в миссии {nickname}</h2>
+                            <h3 class="text-default" style="background-color:  #d63384; font-size: 16px;">Поздравляем! Ваш рейтинг после {level} этапа отбора составляет {rating}!</h3>
+                            <h3 class="text-default" style="background-color:  #fd7e14; font-size: 16px;">Желаем удачи!</h3>
+                          </body>
+                        </html>"""
 
 
 if __name__ == '__main__':
